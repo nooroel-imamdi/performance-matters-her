e8ef7896ac4f671578194009c1f27bc92fc2b5b1
Nooroel Imamdi <nooroelimamdi@gmail.com>
2017-04-11 15:35:42 +0200

    api calls uitgevoerd

--- app.js
+++ app.js
@@ -20,28 +20,28 @@ var searchKey = process.env.SEARCH_KEY;
 
 // View Engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/src/views'));
+app.set('views', path.join(__dirname, './views'));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Set Static Path
-app.use(express.static(path.join(__dirname, './src')));
+app.use(express.static(path.join(__dirname, './public')));
 
 // Routing
 app.get('/', function (req, res) {
-  request(apiUrl + apiKey + searchKey, function (error, response, body) {
+  request(apiUrl + '?q=' + userInput + apiKey + '&format=json', function (error, response, body) {
     var data = JSON.parse(body)
-    res.render('index.ejs', {residences: data})
+    res.render('index.ejs', {paintings: data})
   });
 })
 
-app.get('/residences/:GroupByObjectType', function (req, res, GroupByObjectType) {
-  request(apiUrl + 'detail/' + apiKey + '/koop/' + req.params.GroupByObjectType, function (error, response, body) {
-    console.log(GroupByObjectType);
+app.get('/painting/:objectNumber', function (req, res, objectNumber) {
+  request(apiUrl + '/' + req.params.objectNumber + apiKey + '&format=json', function (error, response, body) {
+    console.log(objectNumber);
     var data = JSON.parse(body)
-    res.render('detail.ejs', {residence: data})
+    res.render('detail.ejs', {paint: data})
   });
 })
 
